9a1086933314ba938e8016373879f3323cb45078
Shashank-maurya1020 <sknickmaurya@gmail.com>
2021-03-04 00:23:26 -0800

    5th change

--- 3. Longest Substring Without Repeating Characters.py
+++ 3. Longest Substring Without Repeating Characters.py
@@ -1,1 +1,21 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+'''
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+'''
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_l = 0
+        for i in range(len(s)):
+            temp = []
+            for j in range(i, len(s)):
+                if s[j] not in set(temp):
+                    temp += [s[j]]
+                    if len(temp) > max_l:
+                        max_l = len(temp)
+                else:
+                    break
+        return max_l
